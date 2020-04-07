1eb3f8a5c0ef520ad008bfdaa1c689cb52d539f5
Joanne Young <joanneyoung_@hotmail.com>
2020-04-07 17:09:56 +0100

    add ammendable cohort

--- exercises.rb
+++ exercises.rb
@@ -12,29 +12,35 @@ def input_students
   name = gets.chomp
   puts "what's their superpower"
   superpower = gets.chomp
+  puts "What is the cohort"
+  cohort = gets.chomp
+
   #while the name is not empty, repeat this code
   while !name.empty? do
-    students << {name: name, superpower: superpower, cohort: :november}
+    students << {name: name, superpower: superpower, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
     #get another name from the user
-    puts "what's the name of the next student?"
+    puts "What's the name of the next student?"
     name = gets.chomp
     puts "and their superpower"
     superpower = gets.chomp
+    puts "what cohort?"
+    cohort = gets.chomp
   end
   #return the array of students
   students
 end
 
 def print(students)
+  
   students.each_with_index do |student, index|
-    puts "#{index}. #{student[:name]}, their superpower is #{student[:superpower]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]}, their superpower is #{student[:superpower]} (#{student[:cohort]} cohort)"
   end
   students
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(5)
 end
 #nothing happens untill we call the methods
 students = input_students
