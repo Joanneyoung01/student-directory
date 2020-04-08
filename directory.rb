9c2a4dd8de77cdaa7ed54b07e357d2f9283828f1
Joanne Young <joanneyoung_@hotmail.com>
2020-04-08 18:54:54 +0100

    completed student directory, with loading file

--- directory.rb
+++ directory.rb
@@ -3,18 +3,31 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
+    try_load_students
   end
 end
 
-def load_students
+def load_students(filename = "students.csv")
   #hidden file is saved in .gitignore folder
-  file = File.open("students.csv", "r")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first #first arguement gets taken
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exit."
+    exit # quit the program
+  end
 end
 
 def print_menu
@@ -54,14 +67,14 @@ def input_students
   puts "To finish, just hit return twice"
   # create an empty array
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   # return the array of students
   @students
