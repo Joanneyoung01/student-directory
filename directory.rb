e616d94c40f96368ea15583ae8c99d3d6b2764cc
Joanne Young <joanneyoung_@hotmail.com>
2020-04-08 17:03:33 +0100

    write students to csv file, update menu

--- directory.rb
+++ directory.rb
@@ -12,6 +12,7 @@ def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save students to file"
   puts "9. Exit"
 end
 
@@ -27,6 +28,8 @@ def process (selection)
     input_students
     when "2"
     show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -65,6 +68,18 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
