6bdc487ad89f345616fa73b72a25fdc9e552986c
Joanne Young <joanneyoung_@hotmail.com>
2020-04-08 13:57:26 +0100

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,39 +1,50 @@
-student_count = 11
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------------"
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+    students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      #this will exit the program and terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create empty array
+  # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  #while the name is not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
+    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    #get another name from the user
-    name = gets.chomp 
+    # get another name from the user
+    name = gets.chomp
   end
-  #return the array of students
+  # return the array of students
   students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
@@ -45,8 +56,5 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-#nothing happens untill we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu
